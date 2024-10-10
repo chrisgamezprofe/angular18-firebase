5dbf894de527ee41135f885e2bde01b25f914c72
Christian Serna <christian.serna@cafedecolombia.com>
2024-10-10 05:14:32 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Firestore, collectionData } from '@angular/fire/firestore';
+import { addDoc, collection, getDocs } from 'firebase/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +13,34 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'proyectodemo';
+  items$: Observable<any[]>;
+  newItem = { dni: '', name: '', email: '' };
+  
+  constructor() {
+    const firestore = inject(Firestore);
+    const teacherCollection = collection(firestore, 'teachers');
+    this.items$ = collectionData(teacherCollection);
+    this.addExampleData(firestore);
+  }
+
+  async addExampleData(firestore: Firestore) {
+    const teacherCollection = collection(firestore, 'teachers');
+    const snapshot = await getDocs(teacherCollection);
+
+    if (snapshot.empty) {
+      const itemExample = {
+        dni: '12345678',
+        name: 'Juan Perez',
+        email: 'juan.perez@example.com',
+      }
+
+      try {
+        await addDoc(teacherCollection, itemExample);
+        alert('Item agregado!!')
+      } catch (error) {
+        console.log('Error!!', error)
+      }
+    }
+    
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase:{
+        apiKey: "AIzaSyBWT2hCwZ4jG9C0WEXZcJzw8hWe0sWeFD0",
+        authDomain: "sistema-academico-4c1ff.firebaseapp.com",
+        projectId: "sistema-academico-4c1ff",
+        storageBucket: "sistema-academico-4c1ff.appspot.com",
+        messagingSenderId: "81510302590",
+        appId: "1:81510302590:web:8de22679b9eac7a84c692f",
+        measurementId: "G-RXGSETZBKJ"
+    }
+}
